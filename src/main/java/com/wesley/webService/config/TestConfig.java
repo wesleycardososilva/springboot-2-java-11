7561fb80ca7e6f33e151a0bcc9059971cd2a729a
wesleycardososilva <silvawesley@protonmail.com>
2020-08-05 22:10:53 -0300

    Payment, one-to-one association

--- src/main/java/com/wesley/webService/config/TestConfig.java
+++ src/main/java/com/wesley/webService/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.wesley.webService.entities.Category;
 import com.wesley.webService.entities.Order;
 import com.wesley.webService.entities.OrderItem;
+import com.wesley.webService.entities.Payment;
 import com.wesley.webService.entities.Product;
 import com.wesley.webService.entities.User;
 import com.wesley.webService.entities.enums.OrderStatus;
@@ -80,6 +81,10 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}
 
 }
