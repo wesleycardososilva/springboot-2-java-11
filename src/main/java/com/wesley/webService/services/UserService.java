b319ecbc027141cd8dcc767002e4428fd4dcbe3e
wesleycardososilva <silvawesley@protonmail.com>
2020-08-10 15:55:56 -0300

    Exception handling - findById

--- src/main/java/com/wesley/webService/services/UserService.java
+++ src/main/java/com/wesley/webService/services/UserService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.wesley.webService.config.exceptions.ResourceNotFoundException;
 import com.wesley.webService.entities.User;
 import com.wesley.webService.repositories.UserRepository;
 
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	
